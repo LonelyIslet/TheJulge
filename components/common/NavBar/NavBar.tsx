fb84ba87dd73f0c687787df90aca08397f3f55fd
Jer1s <flqmbl@gmail.com>
2023-07-07 22:43:09 +0900

    feat: NavBar ui 작성

--- components/common/NavBar/NavBar.tsx
+++ components/common/NavBar/NavBar.tsx
@@ -5,18 +5,36 @@ import styles from "./NavBar.module.scss";
 
 const NavBar = () => {
   return (
-    <nav>
-      <Link href="/">
-        <div className={styles.logo}>
-          <Image
-            fill
-            src="/images/logo.svg"
-            alt="The Julge Logo"
-          />
-        </div>
-      </Link>
-      <SearchBar />
-      <div className={styles.third} />
+    <nav className={styles.container}>
+      <div className={styles.leftItems}>
+        <Link href="/">
+          <div className={styles.logo}>
+            <Image
+              fill
+              src="/images/logo.svg"
+              alt="The Julge Logo"
+              priority
+            />
+          </div>
+        </Link>
+      </div>
+      <div className={styles.searchBar}>
+        <SearchBar
+          placeholder="가게 이름으로 찾아보세요"
+        />
+      </div>
+      <div className={styles.rightItems}>
+        <Link href="/signin">
+          <h2>
+            로그인
+          </h2>
+        </Link>
+        <Link href="signup">
+          <h2>
+            회원가입
+          </h2>
+        </Link>
+      </div>
     </nav>
   );
 };
--- components/common/SearchBar/SearchBar.tsx
+++ components/common/SearchBar/SearchBar.tsx
@@ -1,5 +1,31 @@
-const SearchBar = () => {
-  return <div />;
+import Image from "next/image";
+import styles from "./SearchBar.module.scss";
+
+interface SearchBarProps {
+  className?: string;
+  placeholder?: string;
+}
+
+const SearchBar = ({
+  className = "",
+  placeholder = "",
+}: SearchBarProps) => {
+  return (
+    <form className={`${styles.container} ${className}`}>
+      <input
+        type="search"
+        className={styles.input}
+        placeholder={placeholder}
+      />
+      <div className={styles.icon}>
+        <Image
+          fill
+          src="/images/search.svg"
+          alt="Search Icon"
+        />
+      </div>
+    </form>
+  );
 };
 
 export default SearchBar;
