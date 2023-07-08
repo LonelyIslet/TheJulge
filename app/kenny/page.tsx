c4a585902db751f596a3ba476d326b732f2cd45b
SeyoungCho <wnl383@naver.com>
2023-07-09 02:03:14 +0900

    fix: ModalPortal을 Modal컴포넌트에서 import하도록 수정 JUL-43

--- app/kenny/page.tsx
+++ app/kenny/page.tsx
@@ -2,7 +2,6 @@
 
 import { StatusChip } from "components/common";
 import Modal from "components/common/Modal/Modal";
-import ModalPortal from "components/common/Modal/ModalPortal";
 import { useState } from "react";
 import { ApplyStatus } from "types/enums/apply.enum";
 import { ModalType } from "types/enums/modal.enum";
@@ -14,11 +13,8 @@ const Page = () => {
     <main>
       <StatusChip status={ApplyStatus.PENDING} />
       <button type="button" onClick={() => { setIsModalOpen((prev) => { return !prev; }); }}>Open Modal</button>
-      {isModalOpen && (
-        <ModalPortal>
-          <Modal type={ModalType.ACTION} message="신청을 거절하시겠어요?" onClose={() => { setIsModalOpen(false); }} onClickProceed={() => { setIsModalOpen(false); }} />
-        </ModalPortal>
-      )}
+      {isModalOpen
+        && <Modal type={ModalType.ACTION} message="신청을 거절하시겠어요?" onClose={() => { setIsModalOpen(false); }} onClickProceed={() => { setIsModalOpen(false); }} />}
     </main>
   );
 };
--- components/common/Modal/Modal.tsx
+++ components/common/Modal/Modal.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { ModalType } from "types/enums/modal.enum";
 import styles from "./Modal.module.scss";
+import ModalPortal from "./ModalPortal";
 
 const modalIconSrcMap = {
   [ModalType.ACTION]: "/images/modal-check.svg",
@@ -21,7 +22,7 @@ const Modal = ({
   type, message, onClose, onClickProceed,
 }: ModalProps) => {
   return (
-    <>
+    <ModalPortal>
       <div className={styles.overlay} />
       <div className={styles.container}>
         <div className={styles.modalHeader}>
@@ -35,7 +36,7 @@ const Modal = ({
           {onClickProceed && <button type="button" className={styles.proceedBtn} onClick={onClickProceed}>거절하기</button>}
         </div>
       </div>
-    </>
+    </ModalPortal>
   );
 };
 
