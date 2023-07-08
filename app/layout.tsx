6abc60d75e5944c29a12c2ffeeedb162e4a9007f
SeyoungCho <wnl383@naver.com>
2023-07-09 00:40:56 +0900

    chore: Portal을 위한 modal-root div 추가 JUL-43

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,7 @@ const RootLayout = ({
     <StoreProvider>
       <html lang="ko">
         <body className={spoqaHanSansNeo.className}>
+          <div id="modal-root" />
           <NavBar />
           {children}
         </body>
