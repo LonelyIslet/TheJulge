b3fc9e3cb0e11ae40a866fc8b1a6fc18f71492de
wooki <bwlimtony@gmail.com>
2023-07-09 11:02:17 +0900

    fix: tony Page lint 수정

--- app/tony/Page.tsx
+++ app/tony/Page.tsx
@@ -15,7 +15,9 @@ const Page = () => {
     // console.log(data)
   };
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) => {
+  const handleInputChange = (event:
+  React.ChangeEvent<HTMLInputElement> |
+  React.ChangeEvent<HTMLTextAreaElement>) => {
     const { name, value } = event.target;
     setData((prevData) => {
       return {
@@ -28,11 +30,61 @@ const Page = () => {
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <CustomInput element="text" type="email" label="이메일" placeholder="선택" essential id="email" name="email" validationType="email" data={data} onChange={handleInputChange} />
-        <CustomInput element="text" type="password" label="비밀번호" placeholder="선택" essential id="password" name="password" validationType="password" data={data} onChange={handleInputChange} />
-        <CustomInput element="text" type="password" label="비밀번호 확인" placeholder="선택" essential id="checkingPassword" name="checkingPassword" validationType="checkingPassword" data={data} onChange={(evnet) => { return setData((prev) => { return { ...prev, checkingPassword: evnet.target.value }; }); }} />
-        <CustomInput element="text" type="number" label="시급" placeholder="선택" id="hourlyPay" name="hourlyPay" validationType="hourlyPay" onChange={handleInputChange} />
-        <CustomInput element="text" type="text" label="주소" placeholder="선택" id="address" name="address" onChange={handleInputChange} />
+        <CustomInput
+          element="text"
+          type="email"
+          label="이메일"
+          placeholder="선택"
+          essential
+          id="email"
+          name="email"
+          validationType="email"
+          data={data}
+          onChange={handleInputChange}
+        />
+        <CustomInput
+          element="text"
+          type="password"
+          label="비밀번호"
+          placeholder="선택"
+          essential
+          id="password"
+          name="password"
+          validationType="password"
+          data={data}
+          onChange={handleInputChange}
+        />
+        <CustomInput
+          element="text"
+          type="password"
+          label="비밀번호 확인"
+          placeholder="선택"
+          essential
+          id="checkingPassword"
+          name="checkingPassword"
+          validationType="checkingPassword"
+          data={data}
+          onChange={handleInputChange}
+        />
+        <CustomInput
+          element="text"
+          type="number"
+          label="시급"
+          placeholder="선택"
+          id="hourlyPay"
+          name="hourlyPay"
+          validationType="hourlyPay"
+          onChange={handleInputChange}
+        />
+        <CustomInput
+          element="text"
+          type="text"
+          label="주소"
+          placeholder="선택"
+          id="address"
+          name="address"
+          onChange={handleInputChange}
+        />
         <button type="submit">전송하기</button>
       </form>
     </div>
