8d3371845760784c65c1e47f3303f1c3561069de
wooki <bwlimtony@gmail.com>
2023-07-10 01:40:16 +0900

    feat: input 자동완성 기능 구현

--- app/tony/dropdown/page.tsx
+++ app/tony/dropdown/page.tsx
@@ -1,10 +1,19 @@
+"use client";
+
 import Dropdown from "components/common/Dropdown/Dropdown";
-import React from "react";
+import React, { useState } from "react";
 
 const page = () => {
+  const [data, setData] = useState({});
+  console.log(data);
   return (
     <div>
-      <Dropdown />
+      <Dropdown
+        label="분류"
+        id="category"
+        name="category"
+        onChange={(name, value) => { return setData((prev) => { return { ...prev, [name]: value }; }); }}
+      />
     </div>
   );
 };
--- components/common/Dropdown/Dropdown.tsx
+++ components/common/Dropdown/Dropdown.tsx
@@ -1,12 +1,40 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import React, { useEffect, useState, useRef } from "react";
 import styles from "./Dropdown.module.scss";
 
-const Dropdown = () => {
+interface DropdownProps {
+  label: string
+  id: string
+  name: string
+  onChange: any
+}
+
+const Dropdown = ({
+  label, id, onChange, name,
+}: DropdownProps) => {
   const [toggle, setToggle] = useState(false);
-  const [inputData, setInputData] = useState([]);
-  const [inputValue, setInputValue] = useState("");
+  const [inputData, setInputData] = useState<string[]>();
+  const [inputValue, setInputValue] = useState<string>("");
+
+  const divRef = useRef<HTMLDivElement>(null);
+
+  const oneRef = useRef<HTMLDivElement>(null);
+
+  const handleInputValue = (event) => {
+    setInputValue(event.target.value);
+    onChange(name, event.target.value);
+  };
+
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
+
+  const handlePickData = (e) => {
+    setInputValue(e.target.textContent);
+    onChange(name, e.target.textContent);
+  };
 
   const getDropdownData = async () => {
     const response = await fetch("/data/drop.json");
@@ -16,22 +44,86 @@ const Dropdown = () => {
 
   useEffect(() => {
     getDropdownData();
-  }, [toggle]);
+  }, []);
 
-  console.log(inputData);
+  useEffect(() => {
+    // 현재 document에서 mousedown 이벤트가 동작하면 호출되는 함수입니다.
+    function handleClickOutside(event) {
+      if (divRef.current && !divRef.current.contains(event.target)) {
+        console.log("div 외부 클릭을 감지!");
+      } else {
+        // console.log(oneRef);
+        // setInputValue(oneRef.current);
+      }
+    }
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [oneRef]);
+
   return (
-    <div>
-      {/* <label className={styles.label} htmlFor={id}>{essential ? `${label}*` : label}</label> */}
+    <div className={styles.box}>
+      <label className={styles.label} htmlFor={id}>{label}</label>
       <input
-        onClick={() => { return setToggle(!toggle); }}
+        id={id}
+        className={styles.userInput}
+        name={name}
         value={inputValue}
-        onChange={(e) => { return setInputValue(e.target.value); }}
+        onChange={(e) => { return handleInputValue(e); }}
       />
-      {toggle && (
-      <ul>
-        {!inputValue && inputData?.data?.ward.map((list) => { return <li key={list.ward}>{list}</li>; })}
-      </ul>
-      )}
+      <div ref={divRef} className={styles.container}>
+        {toggle && (
+        <div>
+          {!inputValue
+        && inputData?.data?.ward.map((list) => {
+          return (
+            <button
+              type="button"
+              onClick={handlePickData}
+              className={styles.content}
+              key={list}
+            >
+              {list}
+            </button>
+          );
+        })}
+        </div>
+        )}
+        {inputValue
+        && inputData?.data?.ward?.filter((list) => { return list.includes(inputValue); }).map(((list) => {
+          return (
+            <button
+              key={list}
+              type="button"
+              onClick={handlePickData}
+              className={styles.content}
+            >
+              {list}
+            </button>
+          );
+        }))}
+        {!toggle && (
+        <Image
+          className={styles.toggle}
+          src="/images/dropdown.svg"
+          alt="창내림"
+          width={16}
+          height={16}
+          onClick={handleToggle}
+        />
+        )}
+        {toggle && (
+        <Image
+          className={styles.toggle}
+          src="/images/Triangle.svg"
+          alt="창올림"
+          width={16}
+          height={16}
+          onClick={handleToggle}
+        />
+        )}
+      </div>
     </div>
   );
 };
