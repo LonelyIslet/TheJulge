88b2ca60cc8c3727e5294e17335338aadcee39eb
wooki <bwlimtony@gmail.com>
2023-07-10 22:16:05 +0900

    chore: 공통 컴포넌틑 스타일 수정

--- app/my-page/page.tsx
+++ app/my-page/page.tsx
@@ -1,6 +1,15 @@
+import { CommonLayout } from "components/common";
+
 const page = () => {
   return (
-    <div />
+    <CommonLayout position="above">
+      <header>
+        <h2>프로필</h2>
+      </header>
+      <article>
+        <p>내 프로필을 등록하고 원하는 가게에 지원해 보세요.</p>
+      </article>
+    </CommonLayout>
   );
 };
 
