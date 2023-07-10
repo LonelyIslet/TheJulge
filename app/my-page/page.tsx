b33ba9bc48d298873fd71f4c39f9c8be7fb437eb
wooki <bwlimtony@gmail.com>
2023-07-10 23:44:03 +0900

    feat: 알바생 상세페이지 구현

--- app/my-page/page.tsx
+++ app/my-page/page.tsx
@@ -1,15 +1,10 @@
-import { CommonLayout } from "components/common";
+"use client";
+
+import CommonDetail from "components/common/CommonDetail/CommonDetail";
 
 const page = () => {
   return (
-    <CommonLayout position="above">
-      <header>
-        <h2>프로필</h2>
-      </header>
-      <article>
-        <p>내 프로필을 등록하고 원하는 가게에 지원해 보세요.</p>
-      </article>
-    </CommonLayout>
+    <CommonDetail />
   );
 };
 
---
+++ components/common/CommonDetail/CommonDetail.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import React from "react";
+import { ButtonStyle, ButtonSize } from "types/enums/button.enum";
+import styles from "./CommonDetail.module.scss";
+import CommonLayout from "../CommonLayout/CommonLayout";
+import CommonBtn from "../CommonBtn/CommonBtn";
+
+const CommonDetail = () => {
+  return (
+    <div className={styles.layout}>
+      <CommonLayout position="above">
+        <div>
+          <h2>프로필</h2>
+        </div>
+        <div>
+          <p>내 프로필을 등록하고 원하는 가게에 지원해 보세요.</p>
+          <CommonBtn type={ButtonStyle.SOLID} size={ButtonSize.MEDIUM} message="내 프로필 저장하기" onClick={() => { return console.log("안녕"); }} />
+        </div>
+      </CommonLayout>
+    </div>
+  );
+};
+
+export default CommonDetail;
