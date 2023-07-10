e2bcd47a6c2da5dc60dddf99a54d04534e038fb5
SeyoungCho <wnl383@naver.com>
2023-07-10 22:33:35 +0900

    feat: 로그인페이지에 랜딩이미지, 텍스트 배치 JUL-51

--- app/(auth)/account/page.tsx
+++ app/(auth)/account/page.tsx
@@ -1,11 +1,31 @@
+import Image from "next/image";
 import { AuthForm } from "components/auth";
 import styles from "./page.module.scss";
 
 const AccountPage = () => {
   return (
-    <div className={styles.container}>
-      <AuthForm />
-    </div>
+    <main className={styles.container}>
+      <article>
+        <section className={styles.promotionArea}>
+          <h1>
+            당신의 시간이 가치 있을 수 있도록,
+            <br />
+            <strong>더줄게</strong>
+          </h1>
+          <div style={{ width: "732px", height: "438px", position: "relative" }}>
+            <Image src="/images/landing-img.png" fill alt="랜딩 이미지" />
+          </div>
+          <p>
+            사장님도 알바님도 간편하게 서로를 찾을 수 있게.
+            <br />
+            급한 일손, 단기 알바, 행사 인력 모두 한 곳에서!
+          </p>
+        </section>
+        <section className={styles.formArea}>
+          <AuthForm />
+        </section>
+      </article>
+    </main>
   );
 };
 
