283f82fb62d7984f64fe7d5b6d7603df2139d78b
wooki <bwlimtony@gmail.com>
2023-07-11 00:28:52 +0900

    feat: 상세페이지 완성

--- app/my-page/page.tsx
+++ app/my-page/page.tsx
@@ -1,10 +1,14 @@
 "use client";
 
 import CommonDetail from "components/common/CommonDetail/CommonDetail";
+import { DetailType } from "types/enums/detailPage.enum";
 
 const page = () => {
   return (
-    <CommonDetail />
+    <div>
+      <CommonDetail detailType={DetailType.EMPLOYER} />
+      <CommonDetail detailType={DetailType.EMPLOYEE} />
+    </div>
   );
 };
 
--- components/common/CommonDetail/CommonDetail.tsx
+++ components/common/CommonDetail/CommonDetail.tsx
@@ -6,16 +6,52 @@ import styles from "./CommonDetail.module.scss";
 import CommonLayout from "../CommonLayout/CommonLayout";
 import CommonBtn from "../CommonBtn/CommonBtn";
 
-const CommonDetail = () => {
+type Detail = "EMPLOYER" | "EMPLOYEE";
+
+interface IDeail {
+  EMPLOYER: {
+    titie: string;
+    description: string;
+    buttonText: string;
+  };
+  EMPLOYEE: {
+    titie: string;
+    description: string;
+    buttonText: string;
+  };
+}
+
+interface Icontent {
+  title?: string,
+  description: string,
+  buttonText: string
+}
+
+const CommonDetail = ({ detailType }: { detailType: Detail }) => {
+  const info: IDeail = {
+    EMPLOYER: {
+      titie: "내 가게",
+      description: "내 가게를 소개하고 공고도 등록해 보세요.",
+      buttonText: "가게 등록하기",
+    },
+    EMPLOYEE: {
+      titie: "내 프로필",
+      description: "내 프로필을 등록하고 원하는 가게에 지원해 보세요.",
+      buttonText: "내 프로필 등록하기",
+    },
+  };
+
+  const dataType:Icontent = info[detailType];
+
   return (
     <div className={styles.layout}>
       <CommonLayout position="above">
         <div>
-          <h2>프로필</h2>
+          <h2>{dataType.title}</h2>
         </div>
         <div>
-          <p>내 프로필을 등록하고 원하는 가게에 지원해 보세요.</p>
-          <CommonBtn type={ButtonStyle.SOLID} size={ButtonSize.MEDIUM} message="내 프로필 등록하기" onClick={() => { return console.log("안녕"); }} />
+          <p>{dataType.description}</p>
+          <CommonBtn type={ButtonStyle.SOLID} size={ButtonSize.MEDIUM} message={dataType.buttonText} onClick={() => { return console.log("안녕"); }} />
         </div>
       </CommonLayout>
     </div>
