0d9920975c6adbc9809801aebed117c0e7c7f9ca
SeyoungCho <wnl383@naver.com>
2023-07-12 20:08:53 +0900

    feat: Popover컴포넌트에 top, left, right, bottom prop추가 JUL-41

--- components/common/Popover/Popover.tsx
+++ components/common/Popover/Popover.tsx
@@ -4,15 +4,30 @@ import useClickOutside from "hooks/useClickOutside";
 import React, { useRef } from "react";
 
 interface PopoverProps {
+  top?:number,
+  left?:number,
+  right?:number,
+  bottom?:number,
   onClose: () => void;
   children: React.ReactNode;
 }
 
-const Popover = React.memo(({ onClose, children }: PopoverProps) => {
+const Popover = React.memo(({
+  top, left, right, bottom, onClose, children,
+}: PopoverProps) => {
   const popoverRef = useRef<HTMLDivElement>(null);
   useClickOutside(popoverRef, onClose);
   return (
-    <div style={{ position: "absolute" }} ref={popoverRef}>
+    <div
+      style={{
+        position: "absolute",
+        top: `${top ?? 0}rem`,
+        left: `${left ?? 0}rem`,
+        right: `${right ?? 0}rem`,
+        bottom: `${bottom ?? 0}rem`,
+      }}
+      ref={popoverRef}
+    >
       {children}
     </div>
   );
