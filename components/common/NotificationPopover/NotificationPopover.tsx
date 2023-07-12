2d154c8364892daa3bf27f570ff646d5ce4cbddc
SeyoungCho <wnl383@naver.com>
2023-07-12 18:07:28 +0900

    feat: 알림 모달이 prop으로 알림데이터 받는 기능 추가 JUL-41

--- components/common/NotificationPopover/NotificationPopover.tsx
+++ components/common/NotificationPopover/NotificationPopover.tsx
@@ -1,59 +1,59 @@
+"use client";
+
 import Image from "next/image";
 import classNames from "classnames/bind";
+import { IAlert } from "types/dto";
+import formatTimeRange from "utils/formatTimeRange";
+import { ApplyStatus } from "types/enums/apply.enum";
+import showElapsedTime from "utils/showElapsedTime";
 import styles from "./NotificationPopover.module.scss";
 
 const cx = classNames.bind(styles);
 
-const NotificationPopover = () => {
+const labelMap = {
+  [ApplyStatus.ACCEPTED]: "승인",
+  [ApplyStatus.REJECTED]: "거절",
+};
+
+interface NotificationPopoverProps {
+  alertList: IAlert[];
+}
+
+const NotificationPopover = ({ alertList }: NotificationPopoverProps) => {
   return (
     <div className={styles.container}>
       <div className={styles.header}>
-        <h2>알림 6개</h2>
+        <h2>
+          알림
+          {" "}
+          {alertList.length}
+          개
+        </h2>
         <div className={styles.closeBtn}>
           <Image src="/images/close.svg" fill alt="닫기 버튼" />
         </div>
       </div>
       <ul className={styles.itemList}>
-        <li className={styles.item}>
-          <div className={cx("ellipsis", "accepted")} />
-          <p className={styles.content}>
-            HS 과일주스(2023-01-14 15:00~18:00) 공고 지원이
-            {" "}
-            <span className={styles.accepted}>승인</span>
-            되었어요.
-          </p>
-          <p className={styles.time}>1분 전</p>
-        </li>
-        <li className={styles.item}>
-          <div className={cx("ellipsis", "rejected")} />
-          <p className={styles.content}>
-            HS 과일주스(2023-01-14 15:00~18:00) 공고 지원이
-            {" "}
-            <span className={styles.rejected}>거절</span>
-            되었어요.
-          </p>
-          <p className={styles.time}>1분 전</p>
-        </li>
-        <li className={styles.item}>
-          <div className={cx("ellipsis", "accepted")} />
-          <p className={styles.content}>
-            HS 과일주스(2023-01-14 15:00~18:00) 공고 지원이
-            {" "}
-            <span className={styles.accepted}>승인</span>
-            되었어요.
-          </p>
-          <p className={styles.time}>1분 전</p>
-        </li>
-        <li className={styles.item}>
-          <div className={cx("ellipsis", "accepted")} />
-          <p className={styles.content}>
-            HS 과일주스(2023-01-14 15:00~18:00) 공고 지원이
-            {" "}
-            <span className={styles.accepted}>승인</span>
-            되었어요.
-          </p>
-          <p className={styles.time}>1분 전</p>
-        </li>
+        {alertList.map((item) => {
+          const {
+            shop: { item: shop }, notice: { item: notice }, createdAt, result,
+          } = item;
+          return (
+            <li key={item.id} className={styles.item}>
+              <div className={cx("ellipsis", `${result}`)} />
+              <p className={styles.content}>
+                {shop.name}
+                {`(${formatTimeRange(notice.startsAt, notice.workhour, false)})`}
+                {" "}
+                공고 지원이
+                {" "}
+                <span className={cx(`${result}`)}>{labelMap[result]}</span>
+                되었어요.
+              </p>
+              <p className={styles.time}>{showElapsedTime(createdAt)}</p>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
