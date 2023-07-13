273f5d8da79781e7a74bbed74c7defaa1ac28822
wooki <bwlimtony@gmail.com>
2023-07-13 14:53:09 +0900

    chore: 배포 수정

--- components/common/index.ts
+++ components/common/index.ts
@@ -8,8 +8,18 @@ import HourlyPayPercentage from "./HourlyPayPercentage/HourlyPayPercentage";
 import Modal from "./Modal/Modal";
 import CustomInput from "./CustomInput/CustomInput";
 import CommonBtn from "./CommonBtn/CommonBtn";
+import CommonLayout from "./CommonLayout/CommonLayout";
 
 export {
-  GlobalNav, Pagination, SearchBar, StatusChip,
-  GlobalFooter, PostCard, HourlyPayPercentage, Modal, CustomInput, CommonBtn,
+  GlobalNav,
+  Pagination,
+  SearchBar,
+  StatusChip,
+  GlobalFooter,
+  PostCard,
+  HourlyPayPercentage,
+  Modal,
+  CustomInput,
+  CommonBtn,
+  CommonLayout,
 };
