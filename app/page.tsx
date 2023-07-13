37e9d6e9f95c5b14059b3d11a3cffe36cd1a8eb4
Jer1s <flqmbl@gmail.com>
2023-07-13 14:46:58 +0900

    chore: 충돌 해결 JUL-42

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import { Filter } from "components/common";
 import styles from "./page.module.scss";
 
 const HomePage = () => {
   return (
     <main className={styles.main}>
       <h1>더줄게</h1>
+      <Filter
+        onClose={() => {}}
+      />
     </main>
   );
 };
