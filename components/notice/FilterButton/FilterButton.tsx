6aeb6c400ae819b2d7b5a1ed4f078f6430a47078
Jer1s <flqmbl@gmail.com>
2023-07-15 03:04:21 +0900

    feat: FilterButton ui 생성

--- components/notice/FilterButton/FilterButton.tsx
+++ components/notice/FilterButton/FilterButton.tsx
@@ -1,5 +1,38 @@
+"use client";
+
+import { useState } from "react";
+import { Filter } from "components/common";
+import styles from "./FilterButton.module.scss";
+
 const FilterButton = () => {
-  return <div />;
+  const [isClicked, setIsClicked] = useState(false);
+
+  const onToggle = () => {
+    setIsClicked((prev) => { return !prev; });
+  };
+
+  return (
+    <div className={styles.container}>
+      <button
+        type="button"
+        className={styles.button}
+        onClick={onToggle}
+      >
+        <h2>
+          상세 필터
+          {/* Redux로 필터 갯수 상태관리 */}
+        </h2>
+      </button>
+      {isClicked
+        && (
+          <div className={styles.filterWrapper}>
+            <Filter
+              onClose={onToggle}
+            />
+          </div>
+        )}
+    </div>
+  );
 };
 
 export default FilterButton;
--- components/notice/SortButton/SortButton.tsx
+++ components/notice/SortButton/SortButton.tsx
@@ -1,19 +1,21 @@
 "use client";
 
+import { useState } from "react";
 import Image from "next/image";
 import styles from "./SortButton.module.scss";
 
 const SortButton = () => {
-  const handleClick = () => {
-    // dropdown 열기
-    return null;
+  const [isClicked, setIsClicked] = useState(false);
+
+  const onToggle = () => {
+    setIsClicked((prev) => { return !prev; });
   };
 
   return (
     <button
       type="button"
       className={styles.button}
-      onClick={handleClick}
+      onClick={onToggle}
     >
       <h2>
         마감임박순
@@ -21,7 +23,7 @@ const SortButton = () => {
       <Image
         width={10}
         height={10}
-        src="/images/dropdown.svg"
+        src={isClicked ? "/images/triangle.svg" : "/images/dropdown.svg"}
         alt="Dropdown"
       />
     </button>
