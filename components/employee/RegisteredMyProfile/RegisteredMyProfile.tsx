4d0d91f8e94a2459a81ee0adec116dc257758a5f
wooki <bwlimtony@gmail.com>
2023-07-14 14:19:39 +0900

    feat: 내프로필 데스크탑 버전 구현

--- components/employee/RegisteredMyProfile/RegisteredMyProfile.tsx
+++ components/employee/RegisteredMyProfile/RegisteredMyProfile.tsx
@@ -1,13 +1,36 @@
+import { CommonBtn } from "components/common";
+import Image from "next/image";
+import Link from "next/link";
+import { ButtonStyle } from "types/enums/button.enum";
 import styles from "./RegisteredMyProfile.module.scss";
 
 const RegisteredMyProfile = () => {
   return (
     <div className={styles.container}>
-      <div>
+      <div className={styles.blank}>
         <h2>내 프로필</h2>
       </div>
-      <div>
-        내 프로필 내역
+      <div className={styles.userInfoContainer}>
+        <div className={styles.userInfoDetail}>
+          <p>이름</p>
+          <h2>김승우</h2>
+          <div className={styles.phone}>
+            <Image src="/images/phone.svg" alt="전화번호" width={16} height={20} />
+            <span>010-1234-4223</span>
+          </div>
+          <div className={styles.address}>
+            <Image src="/images/location-red.svg" alt="주소" width={16} height={20} />
+            <span>선호 지역: 서울시 도봉구</span>
+          </div>
+          <p>
+            열심히 일하겠습니다.
+          </p>
+        </div>
+        <div className={styles.wrapperButton}>
+          <Link href="/my-profile/edit">
+            <CommonBtn type="button" style={ButtonStyle.OUTLINE}>편집하기</CommonBtn>
+          </Link>
+        </div>
       </div>
     </div>
   );
