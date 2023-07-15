47210216440fd1542f8cb00050b0f4a11dec39c0
wooki <bwlimtony@gmail.com>
2023-07-15 16:27:03 +0900

    chore: rebase-2

--- app/notice/page.tsx
+++ app/notice/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { CommonLayout, NoticeCard, CommonBtn } from "components/common";
+import EmployeeNotice from "components/employee/EmployeeNotice/EmployeeNotice";
 import styles from "./page.module.scss";
 
 const NoticePage = () => {
@@ -9,31 +10,7 @@ const NoticePage = () => {
   // 3. 데이터 응답에서 온 type을 받아 사장과 알바생을 구분하여 페이지를 제작해야됨
   const type: "employee" | "employer" = "employee";
   return (
-    <div className={styles.top}>
-      <CommonLayout position="above">
-        <div>
-          <p>식당</p>
-          <h2>도토리 식당</h2>
-        </div>
-        <div>
-          <NoticeCard
-            hourlyPay={15000}
-            startsAt="2023-09-14T18:00:00.000Z"
-            address1="서울시 송파구"
-            imageUrl="https://dimg.donga.com/wps/NEWS/IMAGE/2021/01/17/104953245.2.jpg"
-            shopDescription="알바하기 편한 너구리네 라면집!
-            라면 올려두고 끓이기만 하면 되어서 쉬운 편에 속하는 가게입니다."
-            noticeDescription="기존 알바 친구가 그만둬서 새로운 친구를 구했는데, 그 사이에 하루가 비네요.
-            급해서 시급도 높였고 그렇게 바쁜 날이 아니라서 괜찮을거예요."
-            closed
-            workhour={3}
-            originalHourlyPay={10000}
-          >
-            <CommonBtn>신청하기</CommonBtn>
-          </NoticeCard>
-        </div>
-      </CommonLayout>
-    </div>
+    <EmployeeNotice />
   );
 };
 
