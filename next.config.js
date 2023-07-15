8c7c9a5ec2859e095fdd7b9d345203da2c682b21
SeyoungCho <wnl383@naver.com>
2023-07-16 03:28:09 +0900

    chore: 오타 수정 JUL-89

--- next.config.js
+++ next.config.js
@@ -15,7 +15,7 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBBLIC_API_BASE_URL}:path*`,
+        destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}:path*`,
       },
     ];
   },
