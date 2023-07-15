ca2a1a1c67a0ad25b3b7fcaf3b8e57bc2c8c292d
redcoin96 <redcoin96@naver.com>
2023-07-15 15:34:35 +0900

    refactor: interface명 명시적으로 변경

--- app/my-shop/page.tsx
+++ app/my-shop/page.tsx
@@ -6,8 +6,8 @@ import noticeData from "constants/mock/notice.json";
 import noticeList from "constants/mock/noticeList.json";
 import styles from "@/page.module.scss";
 
-const Page = () => {
-  interface INoticeWithTypeInfo extends INotice {
+const MyShopPage = () => {
+  interface INoticeWithClosedInfo extends INotice {
     id: string,
     closed: boolean,
   }
@@ -25,15 +25,14 @@ const Page = () => {
   } = noticeData.item;
 
   const shop = {
-    id: "02479698-8aeb-4c72-9a20-8381148c11c9",
     name: "악마",
     address1: "서울시 강서구 ",
     imageUrl: "https://images.unsplash.com/photo-1630906086851-65a063a8cdd5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1665&q=80",
     originalHourlyPay: 2,
   };
 
-  const notice: INoticeWithTypeInfo[] = (
-    noticeList.items as { item: INoticeWithTypeInfo }[]).map(({ item }) => { return item; });
+  const notice: INoticeWithClosedInfo[] = (
+    noticeList.items as { item: INoticeWithClosedInfo }[]).map(({ item }) => { return item; });
 
   return (
     <main className={styles.main}>
@@ -76,11 +75,10 @@ const Page = () => {
           address={shop.address1}
           imageUrl={shop.imageUrl}
           originalHourlyPay={originalHourlyPay}
-          shopId={shop.id}
         />
       </CommonLayout>
     </main>
   );
 };
 
-export default Page;
+export default MyShopPage;
