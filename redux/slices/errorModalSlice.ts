b6b5fb66b9983be1ab8c93f4add428a680155ec0
SeyoungCho <wnl383@naver.com>
2023-07-16 23:34:03 +0900

    feat: 전역 에러 모달 컴포넌트를 위한 전역상태  추가 JUL-102

---
+++ redux/slices/errorModalSlice.ts
@@ -0,0 +1,28 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface IErrorModalState {
+  isShowing: boolean;
+  message: string;
+}
+
+const initialState: IErrorModalState = {
+  isShowing: false,
+  message: "",
+};
+
+export const errorModalSlice = createSlice({
+  name: "toast",
+  initialState,
+  reducers: {
+    setShowModal: (state, action: PayloadAction<boolean>) => {
+      state.isShowing = action.payload;
+    },
+    setErrorModalMessage: (state, action: PayloadAction<string>) => {
+      state.message = action.payload;
+    },
+  },
+});
+
+export const { setShowModal, setErrorModalMessage } = errorModalSlice.actions;
+
+export default errorModalSlice.reducer;
