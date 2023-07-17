21d4029b17ca855193c383ca994ba3db20d1f33d
wooki <bwlimtony@gmail.com>
2023-07-17 17:11:31 +0900

    fix: eye toggle 위치 변경

--- components/employer/EditNoticeForm/EditNoticeForm.tsx
+++ components/employer/EditNoticeForm/EditNoticeForm.tsx
@@ -1,13 +1,27 @@
 "use client";
 
 import { FormEvent, useState } from "react";
-import { CommonBtn, CustomInput } from "components/common";
+import { CommonBtn, CustomInput, InputNumber } from "components/common";
 import { ButtonSize, ButtonStyle } from "types/enums/button.enum";
+import { ValidationTarget } from "types/enums/inputValidation.enum";
 import styles from "./EditNoticeForm.module.scss";
 
 const EditNoticeForm = () => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [data, setData] = useState({});
+  const [rendering, setRendering] = useState(false);
+
+  const [countValidation, setCountValidation] = useState({
+    hourlyPay: 0,
+    startsAt: 0,
+    workhour: 0,
+    description: 0,
+  });
+
+  const [data, setData] = useState({
+    hourlyPay: "", // 넘버 타입
+    startsAt: "", // 문자열
+    workhour: "", // 넘버
+    description: "", // 문자열
+  });
 
   const handleData = (event:
   React.ChangeEvent<HTMLInputElement |
@@ -34,41 +48,58 @@ const EditNoticeForm = () => {
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
+    setRendering(!rendering);
+    setCountValidation({
+      hourlyPay: 1,
+      startsAt: 1,
+      workhour: 1,
+      description: 1,
+    });
   };
 
   return (
     <div className={styles.layout}>
       <form onSubmit={handleSubmit}>
         <div className={styles.inputBox}>
-          <CustomInput
-            element="text"
-            type="number"
+          <InputNumber
             label="시급"
             placeholder="입력"
             id="hourlyPay"
             name="hourlyPay"
-            required
             onChange={handleData}
+            required
+            unit="원"
+            validationTarget={ValidationTarget.HOURLY_PAY}
+            rendering={rendering}
+            countValidation={countValidation}
+            setCountValidation={setCountValidation}
           />
           <CustomInput
-            element="text"
-            type="text"
             label="시작 일시"
             placeholder="입력"
+            element="text"
             id="startsAt"
             name="startsAt"
-            required
-            onChange={handleData}
-          />
-          <CustomInput
-            element="text"
             type="text"
+            onChange={handleData}
+            required
+            rendering={rendering}
+            validationTarget={ValidationTarget.REQUIRED}
+            countValidation={countValidation}
+            setCountValidation={setCountValidation}
+          />
+          <InputNumber
             label="업무 시간"
             placeholder="입력"
             id="workhour"
             name="workhour"
-            required
             onChange={handleData}
+            required
+            unit="시간"
+            validationTarget={ValidationTarget.REQUIRED}
+            rendering={rendering}
+            countValidation={countValidation}
+            setCountValidation={setCountValidation}
           />
         </div>
         <CustomInput
