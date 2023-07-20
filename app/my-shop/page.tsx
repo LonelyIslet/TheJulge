bda240d5a10a900d78a3a278163368d56d2c9652
redcoin96 <redcoin96@naver.com>
2023-07-20 14:55:13 +0900

    feat: my-shop api 호출

--- app/my-shop/page.tsx
+++ app/my-shop/page.tsx
@@ -1,44 +1,86 @@
 "use client";
 
+import React, {
+  useState, useEffect, useCallback, useRef, SetStateAction,
+} from "react";
 import { MyShop, MyNotice } from "components/employer";
 import { INotice } from "types/dto";
 import { withAuth, withUserType } from "components/hocs";
 import { UserType } from "types/enums/user.enum";
 import noticeList from "constants/mock/noticeList.json";
+import useAppSelector from "redux/hooks/useAppSelector";
+import { useGetUserInfoQuery } from "redux/api/userApi";
+import { CommonDetail, Loader } from "components/common";
+import { DetailType } from "types/enums/detailPage.enum";
+import { useGetNoticesByShopIdQuery, useGetNoticesQuery } from "redux/api/noticeApi";
 import styles from "./page.module.scss";
+
+interface IShopCard {
+  name: string,
+  address1: string,
+  imageUrl: string,
+  description: string,
+  category: string,
+}
+
+interface IData {
+  [key: string]: string | number;
+}
 
 interface INoticeWithClosedInfo extends INotice {
   id: string,
   closed: boolean,
 }
 
-const MyShopPage = () => {
-  const notice: INoticeWithClosedInfo[] = (
-    noticeList.items as { item: INoticeWithClosedInfo }[]).map(({ item }) => { return item; });
+// /shops/{shop_id}/notices
 
-  const shop = {
-    name: "악마",
-    address1: "서울시 강서구 ",
-    imageUrl: "https://images.unsplash.com/photo-1630906086851-65a063a8cdd5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1665&q=80",
-    category: "카페",
-    description: "카페 설명임",
-    originalHourlyPay: 20000,
-  };
+const MyShopPage = () => {
+  const [shopData, setShopData] = useState<IData | null>(null);
+  const [noticeData, setNoticeData] = useState([]);
+  const user = useAppSelector((state) => { return state.user; });
+  const { userInfo } = user;
+  const userId = userInfo?.id as string;
+  const { data, isLoading } = useGetUserInfoQuery(userId);
+  // const { data: noticeData } = useGetNoticesQuery(shopId);
+  const { data: NData, isLoading: noticeIsLoading } = useGetNoticesQuery(shopData?.id, { offset: 0 });
+
+  useEffect(() => {
+    if (!isLoading && !noticeIsLoading && data?.item.shop) {
+      const initialShopData = {
+        id: data.item.shop.item.id,
+        name: data.item.shop.item.name,
+        address1: data.item.shop.item.address1,
+        imageUrl: data.item.shop.item.imageUrl,
+        description: data.item.shop.item.description,
+        category: data.item.shop.item.category,
+      };
+      setShopData(initialShopData as SetStateAction<IShopCard>);
+      setNoticeData(NData?.items.map(({ item }) => { return item; }) as INotice[]);
+    }
+  }, [data, isLoading, NData]);
+
+  console.log(noticeData);
 
   return (
     <div>
       <div className={styles.top}>
-        <MyShop
-          name={shop.name}
-          address={shop.address1}
-          imageUrl={shop.imageUrl}
-          description={shop.description}
-          category={shop.category}
-        />
+        {
+          shopData
+            ? (
+              <MyShop
+                name={shopData.name}
+                address={shopData.address1}
+                imageUrl={shopData.imageUrl}
+                description={shopData.description}
+                category={shopData.category}
+              />
+            )
+            : <CommonDetail detailType={DetailType.EMPLOYER} />
+        }
       </div>
       <div className={styles.bottom}>
         <MyNotice
-          noticeList={notice}
+          noticeList={noticeData}
         />
       </div>
     </div>
