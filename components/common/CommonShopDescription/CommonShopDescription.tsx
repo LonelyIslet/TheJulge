e096bd45a3205a911af3487db4fa94950f26034b
wooki <bwlimtony@gmail.com>
2023-07-20 10:26:14 +0900

    chore: 배포 오류 수정

--- components/common/CommonShopDescription/CommonShopDescription.tsx
+++ components/common/CommonShopDescription/CommonShopDescription.tsx
@@ -16,9 +16,9 @@ const CommonShopDescription = ({ user }: { user: IUser | undefined }) => {
   const router = useParams();
   const [noticeInfo, setNoticeInfo] = useState<INotice>();
 
-  const { data: shopInfo, isLoading, isError } = useGetNoticeByShopAndNoticeIdQuery({
+  const { data: shopInfo, isLoading } = useGetNoticeByShopAndNoticeIdQuery({
     shopId: router.shopId,
-    noticeId: router.noticesId,
+    noticeId: router.noticeId,
   });
 
   // 알바 공고내역 보면서 신청하기 or 편집하기
@@ -39,8 +39,22 @@ const CommonShopDescription = ({ user }: { user: IUser | undefined }) => {
     setNoticeInfo(item);
   }, [shopInfo]);
 
-  if (isError) {
-    return null;
+  const userType = user?.type;
+  let btnType;
+
+  if (userType === ("employer" || undefined) || noticeInfo?.closed) {
+    btnType = (
+      <CommonBtn
+        onClick={handleApplyJob}
+        style={ButtonStyle.DISABLE}
+      >
+        신청 불가
+      </CommonBtn>
+    );
+  } else if (user?.shop?.item.id === shopInfo?.item.id) {
+    btnType = <CommonBtn onClick={handleEditNotice}>편집하기</CommonBtn>;
+  } else {
+    btnType = <CommonBtn onClick={handleApplyJob}>신청하기</CommonBtn>;
   }
 
   let renderingUi;
@@ -64,17 +78,8 @@ const CommonShopDescription = ({ user }: { user: IUser | undefined }) => {
             workhour={noticeInfo.workhour}
             originalHourlyPay={noticeInfo?.shop?.item.originalHourlyPay}
           >
-            {(noticeInfo?.closed || user?.type === "employer" || user?.type === undefined) && (
-            <CommonBtn
-              onClick={handleApplyJob}
-              style={ButtonStyle.DISABLE}
-            >
-              신청 불가
-            </CommonBtn>
-            )}
-            {!noticeInfo?.closed && user?.type === "employee"
-              ? <CommonBtn onClick={handleApplyJob}>신청하기</CommonBtn>
-              : <CommonBtn onClick={handleEditNotice}>편집하기</CommonBtn>}
+
+            {btnType}
           </NoticeCard>
         </div>
       </CommonLayout>
