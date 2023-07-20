03593fc945b080b0a219d72fea1834b052219f0c
redcoin96 <redcoin96@naver.com>
2023-07-21 02:56:51 +0900

    feat: api 내보내기추가

--- redux/slices/userSlice.ts
+++ redux/slices/userSlice.ts
@@ -27,6 +27,6 @@ export const userSlice = createSlice({
   },
 });
 
-export const { setUser } = userSlice.actions;
+export const { setUser, setUserShop } = userSlice.actions;
 
 export default userSlice.reducer;
