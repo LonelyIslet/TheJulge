ab19155d058bd3d0f38a42b1d71952875ed9fe0b
Jer1s <flqmbl@gmail.com>
2023-07-21 00:39:18 +0900

    refactor: NoticesPage 수정 JUL-112

--- app/notices/page.tsx
+++ app/notices/page.tsx
@@ -1,7 +1,20 @@
 import { HomePageMain } from "components/notice";
+import { Address1 } from "types/shop/address";
+import { Sort } from "types/notice/queries";
+import LIMIT from "constants/notice/options/LIMIT";
+import convertToArray from "utils/convertToArray";
+import getFirstValue from "utils/getFirstValue";
 import getNotices from "utils/api/getNotices";
-import { SortOptions } from "types/enums/sort.enum";
 import styles from "./page.module.scss";
+
+interface IProcessedParams {
+  page?: number,
+  keyword?: string,
+  sort?: Sort,
+  address?: Address1[],
+  startsAtGte?: string,
+  hourlyPayGte?: number,
+}
 
 interface NoticesPageProps {
   searchParams: {
@@ -12,22 +25,41 @@ interface NoticesPageProps {
 const NoticesPage = async ({
   searchParams,
 }: NoticesPageProps) => {
-  const { keyword, sort, filter } = searchParams;
-  const noticeList = await getNotices({ keyword, sort, filter });
-  let sortOptionId = 0;
+  const processedParams: IProcessedParams = {
+    page: Number(getFirstValue(searchParams.page)) || undefined,
+    keyword: getFirstValue(searchParams.keyword),
+    sort: getFirstValue(searchParams.sort) as Sort,
+    address: convertToArray(searchParams.address) as Address1[],
+    startsAtGte: getFirstValue(searchParams.startsAtGte),
+    hourlyPayGte: Number(getFirstValue(searchParams.hourlyPayGte)) || undefined,
+  };
 
-  switch (sort) {
-    case SortOptions.PAY:
-      sortOptionId = 1;
-      break;
-    case SortOptions.HOUR:
-      sortOptionId = 2;
-      break;
-    case SortOptions.SHOP:
-      sortOptionId = 3;
-      break;
-    default:
-      break;
+  const {
+    page,
+    keyword,
+    sort,
+    address,
+    startsAtGte,
+    hourlyPayGte,
+  } = processedParams;
+
+  const { count, noticeList } = await getNotices({
+    page,
+    keyword,
+    sort,
+    address,
+    startsAtGte,
+    hourlyPayGte,
+  });
+
+  let currentPage = 1;
+  let lastPage = 1;
+  if (page) {
+    currentPage = page;
+  }
+
+  if (count) {
+    lastPage = Math.floor((count - 1) / LIMIT) + 1;
   }
 
   return (
@@ -35,9 +67,14 @@ const NoticesPage = async ({
       <div className={styles.bottom}>
         <HomePageMain
           noticeList={noticeList}
+          limit={LIMIT}
+          currentPage={currentPage}
+          lastPage={lastPage}
           keyword={keyword}
-          sortOptionId={sortOptionId}
-          filter={filter}
+          sort={sort}
+          address={address}
+          startsAtGte={startsAtGte}
+          hourlyPayGte={hourlyPayGte}
         />
       </div>
     </div>
